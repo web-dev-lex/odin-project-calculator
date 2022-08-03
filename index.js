38b8a494b7013d5136ee56ebac8d4d680876bac5
Lex <webdevlex35@gmail.com>
2022-08-02 21:54:56 -0400

    created UI

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 
 
-
-
 function add(a, b) {
   let sum = a + b;
   return sum;
 }
-
-
-
-
 
 function subtract(a, b) {
   let difference = a - b;
   return difference;
 }
 
-
-
 function multiply(a, b) {
   let product = a * b;
   return product;
 }
 
-
-
 function divide(a, b) {
   let quotient = a / b;
   return quotient;
 }
-
-
 
 function operate() {
   let operator = prompt("");
