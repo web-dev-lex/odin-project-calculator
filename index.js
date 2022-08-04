caea0c53f29d678f7675e8ec37de9bae60667b3b
Lex <webdevlex35@gmail.com>
2022-08-03 22:04:41 -0400

    working on functions

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 
-
+// addition function
 function add(a, b) {
   let sum = a + b;
   return sum;
 }
 
+// subtraction function
 function subtract(a, b) {
   let difference = a - b;
   return difference;
 }
 
+// multiplication function
 function multiply(a, b) {
   let product = a * b;
   return product;
 }
 
+// division function
 function divide(a, b) {
   let quotient = a / b;
   return quotient;
 }
 
+// operation function
 function operate() {
   let operator = prompt("");
   let input1 = prompt("");
@@ -27,16 +31,41 @@ function operate() {
   let a = parseInt(input1);
   let b = parseInt(input2);
   if (operator === "+") {
-    alert(add(a, b));
+    return add(a, b);
   } else if (operator === "-") {
-    alert(subtract(a, b));
+    return subtract(a, b);
   } else if (operator === "*") {
-    alert(multiply(a, b));
+    return multiply(a, b);
   } else if (operator === "/") {
-    alert(divide(a, b));
+    return divide(a, b);
   } else {
-    alert("Invalid");
+    return ("Invalid");
   }
 }
 
-operate();
+// global variables
+const zero = document.getElementById('zero');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const display = document.getElementById('display');
+
+const buttons = [
+  {name: zero, value: 0},
+  {name: one, value: 1},
+  {name: two, value: 2},
+  {name: three, value: 3},
+  {name: four, value: 4},
+  {name: five, value: 5},
+  {name: six, value: 6},
+  {name: seven, value: 7},
+  {name: eight, value: 8},
+  {name: nine, value: 9},
+];
+
